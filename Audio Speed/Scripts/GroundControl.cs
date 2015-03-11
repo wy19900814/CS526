47b50daccda2c15aff9bd8625a8abec190a76dd4
yanfjz <student@eggimac12.usc.edu>
2015-03-10 22:03:24 -0700

    menu

---
+++ Audio Speed/Scripts/GroundControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	//Material texture offset rate
+	float speed = .5f;
+	
+	//Offset the material texture at a constant rate
+	void Update () {
+		float offset = Time.time * speed;                             
+		renderer.material.mainTextureOffset = new Vector2(0, -offset); 
+	}
+}
